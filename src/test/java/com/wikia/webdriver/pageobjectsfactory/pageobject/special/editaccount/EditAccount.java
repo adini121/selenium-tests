e3e775321615c8b337693d1b4ba8417849dee3cf
Ludwik Kaźmierczak <ludwik.kazmierczak@gmail.com>
2015-02-05 10:03:29 +0100

    Added cookie log

--- src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/special/editaccount/EditAccount.java
+++ src/test/java/com/wikia/webdriver/pageobjectsfactory/pageobject/special/editaccount/EditAccount.java
@@ -4,6 +4,7 @@ import com.wikia.webdriver.common.contentpatterns.URLsContent;
 import com.wikia.webdriver.common.logging.PageObjectLogging;
 import com.wikia.webdriver.pageobjectsfactory.pageobject.BasePageObject;
 
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -13,6 +14,10 @@ import org.openqa.selenium.support.FindBy;
  */
 public class EditAccount extends BasePageObject {
 
+  private static final String
+      USER_ACCOUNT_REOPEN_MESSAGE =
+      "Successfully removed disabled bit for account";
+  private static final String USER_ACCOUNT_CLOSED_MESSAGE = "Successfully disabled account";
   @FindBy(css = "[name=wpUserName]")
   private WebElement userNameField;
   @FindBy(css = "[value='Close account']")
@@ -28,17 +33,19 @@ public class EditAccount extends BasePageObject {
   @FindBy(css = "fieldset > span")
   private WebElement statusMessage;
 
-  private static final String
-      USER_ACCOUNT_REOPEN_MESSAGE =
-      "Successfully removed disabled bit for account";
-  private static final String USER_ACCOUNT_CLOSED_MESSAGE = "Successfully disabled account";
-
   public EditAccount(WebDriver driver, String communityWikiURL, String userName) {
     super(driver);
     driver.get(
         communityWikiURL +
         URLsContent.SPECIAL_EDIT_ACCOUNT
     );
+    for (Cookie cookie : driver.manage().getCookies()) {
+      PageObjectLogging.log(
+          "COOKIE: ",
+          cookie.toString(),
+          true
+      );
+    }
     userNameField.sendKeys(userName);
     userNameField.submit();
     PageObjectLogging.log(
